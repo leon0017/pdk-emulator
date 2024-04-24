543aacd8e4ba761d40aef132e735bfc09a5e97c5
leon0017 <leonrobi@proton.me>
2024-04-24 23:27:34 +0100

    TODO and give magic number a const val

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod time;
 
 const TEST_PROGRAM: &str = "TestProgram_PFS154.bin";
 const CLOCK_HZ: f64 = 1_000_000.0;
+const NANOS_IN_SEC: f64 = 1_000_000_000.0;
 
 fn main() {
     // TODO: Prettier error logging instead of unwrapping
@@ -17,17 +18,18 @@ fn main() {
     let mut start_nanos = sys_nanos().unwrap();
 
     #[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
-    let nano_sleep = ((1.0 / CLOCK_HZ) * 1_000_000_000.0) as u128;
+    let nano_sleep = ((1.0 / CLOCK_HZ) * NANOS_IN_SEC) as u128;
 
+    // TODO: Clock loop could probably be improved, figure out a way to use sleep instead of recursively checking time
     let mut i: i64 = 0;
     loop {
         let now_nanos = sys_nanos().unwrap();
         if (now_nanos - start_nanos) > nano_sleep {
-            i += 1;
-
             if i % 1_000_000 == 0 {
                 println!("Clock has ticked: {i} times");
             }
+
+            i += 1;
 
             start_nanos = now_nanos;
         }
